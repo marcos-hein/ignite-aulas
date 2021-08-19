5df175017ab19855627e8af4e03e86adef0723c0
Marcos Pereira <marcos.hen-ext@via.com.br>
2021-08-19 11:21:27 -0300

    Conceitos importantes

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,5 +1,11 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
-  return <h1>Hello, React!</h1>
+  return (
+    <>
+      <RepositoryList />
+    </>
+    )
+
 }
