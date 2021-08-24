f5bc6e72d230b1dd57dad21f9a98e3c8c5e5e96a
Marcos Pereira <marcos.hen-ext@via.com.br>
2021-08-24 10:19:34 -0300

    Criando e estilizando botôes de Tipo

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
-import { Container } from "./styles";
+import outcomeImg from "../../assets/outcome.svg";
+import incomeImg from "../../assets/income.svg";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -8,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal 
         isOpen={isOpen}
@@ -34,6 +39,28 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
             type="number"
             placeholder="Valor"
           />
+
+          <TransactionTypeContainer>
+            <RadioBox
+              type="button"
+              onClick={() => { setType('deposit') }}
+              isActive={type === 'deposit'}
+              activeColor="green"
+            >
+              <img src={incomeImg} alt="Entrada" />
+              <span>Entrada</span>
+            </RadioBox>
+
+            <RadioBox
+              type="button"
+              onClick={() => { setType('withdraw') }}
+              isActive={type === 'withdraw'}
+              activeColor="red"
+            >
+              <img src={outcomeImg} alt="Saída" />
+              <span>Saída</span>
+            </RadioBox>
+          </TransactionTypeContainer>
 
           <input
             placeholder="Categoria"
